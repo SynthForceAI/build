b95fa56f6e84a419c8a0f116435c2a8e49e421bd
samarthkamblisk-glitch <samarthkambli.sk@gmail.com>
2026-05-22 16:32:33 -0400

    Fix next.config.js: remove deprecated swcMinify and eslint options

--- next.config.js
+++ next.config.js
@@ -1,13 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  typescript: {
-    ignoreBuildErrors: false,
-  },
-  eslint: {
-    ignoreDuringBuilds: false,
-  },
-};
-
-module.exports = nextConfig;
+module.exports = { reactStrictMode: true };
