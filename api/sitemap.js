fb816ff9c92273fea1cd2fda59a229bdd9511493
samarthkamblisk-glitch <samarthkambli.sk@gmail.com>
2026-05-22 13:32:12 -0400

    cleanup: remove old static site files from main (HTML, assets, configs)

--- api/sitemap.js
+++
@@ -1,9 +0,0 @@
-const { readFileSync } = require('fs');
-const { join } = require('path');
-
-module.exports = (req, res) => {
-  const filePath = join(process.cwd(), 'sitemap.xml');
-  const xml = readFileSync(filePath, 'utf8');
-  res.setHeader('Content-Type', 'application/xml; charset=utf-8');
-  res.send(xml);
-};
